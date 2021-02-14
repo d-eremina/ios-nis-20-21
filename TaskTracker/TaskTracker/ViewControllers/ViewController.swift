7c93e143f7d98ce6ac7107d9acda0ff896f9d0ca
Daria <dveremina@edu.hse.ru>
2021-02-14 14:22:35 +0300

    searchbar

--- TaskTracker/TaskTracker/ViewControllers/ViewController.swift
+++ TaskTracker/TaskTracker/ViewControllers/ViewController.swift
@@ -8,15 +8,18 @@
 import CoreData
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchResultsUpdating, UISearchBarDelegate {
     @IBOutlet var tableView: UITableView!
+    let searchController = UISearchController()
     
     var tasks: [NSManagedObject] = []
+    var filteredTasks: [NSManagedObject] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        initSearchController()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -24,6 +27,19 @@ class ViewController: UIViewController {
       updateTasks()
     }
 
+    
+    func initSearchController() {
+        searchController.loadViewIfNeeded()
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.enablesReturnKeyAutomatically = false
+        searchController.searchBar.returnKeyType = UIReturnKeyType.done
+        
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.searchBar.scopeButtonTitles = ["All", "New", "In Progress", "Done"]
+        searchController.searchBar.delegate = self
+    }
     
     func updateTasks() {
         tasks.removeAll()
@@ -42,6 +58,27 @@ class ViewController: UIViewController {
           tasks = try managedContext.fetch(fetchRequest)
         } catch let error as NSError {
           print("Could not fetch. \(error), \(error.userInfo)")
+        }
+        tableView.reloadData()
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchBar = searchController.searchBar
+        let scopeButton = searchBar.scopeButtonTitles![searchBar.selectedScopeButtonIndex]
+        let searchText = searchBar.text!
+        filterForSearchTextAndScopeButton(searchText: searchText, scopeButton: scopeButton)
+    }
+    
+    func filterForSearchTextAndScopeButton(searchText: String, scopeButton: String = "All") {
+        filteredTasks = tasks.filter {
+            task in
+            let scopeMatch = (scopeButton == "All" || (((task as! Task).heading?.lowercased().contains(scopeButton.lowercased())) != nil))
+            if (searchController.searchBar.text != "") {
+                let searchTextMatch = (task as! Task).heading?.lowercased().contains(searchText.lowercased())
+                return scopeMatch && searchTextMatch!
+            } else {
+                return scopeMatch
+            }
         }
         tableView.reloadData()
     }
@@ -71,9 +108,16 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         tableView.deselectRow(at: indexPath, animated: true)
         
         let viewController = storyboard?.instantiateViewController(identifier: "Task") as! TaskViewController
-        viewController.title = (tasks[indexPath.row] as! Task).heading
+        
+        let currentTask: Task
+        if searchController.isActive {
+            currentTask = filteredTasks[indexPath.row] as! Task
+        } else {
+            currentTask = tasks[indexPath.row] as! Task
+        }
+        
+        viewController.title = currentTask.heading
         _ = viewController.view
-        let currentTask = tasks[indexPath.row] as! Task
         viewController.label.text = currentTask.heading
         viewController.annotation.text = currentTask.annotation
         viewController.date.text = currentTask.date
@@ -83,12 +127,22 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if searchController.isActive {
+            return filteredTasks.count
+        }
         return tasks.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let task = tasks[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let task: NSManagedObject!
+        if searchController.isActive {
+            task = filteredTasks[indexPath.row]
+        } else {
+            task = tasks[indexPath.row]
+        }
+        
         cell.textLabel?.text = task.value(forKeyPath: "heading") as? String
         cell.detailTextLabel?.text = task.value(forKeyPath: "date") as? String
         return cell
@@ -96,7 +150,12 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            let commit = tasks[indexPath.row]
+            let commit: NSManagedObject!
+            if searchController.isActive {
+                commit = filteredTasks[indexPath.row]
+            } else {
+                commit = tasks[indexPath.row]
+            }
             
             guard let appDelegate =
                     UIApplication.shared.delegate as? AppDelegate else {
